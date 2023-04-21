9e82a83cbd82003782c2d1a475446fa2fefc62d8
Shalva Mist <shalvamist@microsoft.com>
2023-04-21 09:59:40 -0700

    Changed the history mode to hash - to support static server deployment

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "../components/Home.vue";
 import EmotionRecognition from "../components/models/Emotion.vue";
@@ -45,7 +45,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
